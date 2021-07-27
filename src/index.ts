eb4a54ae9bb9a373f6a31acb130ed20025fa004e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-27 19:31:55 +0200

    Create express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
