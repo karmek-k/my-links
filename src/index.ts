3b884c6394f7b631247cae1441f1c31e8199e69f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-28 13:36:44 +0200

    Create user route

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import UserRouter from './routes/user';
+
 const port = process.env.PORT || 8000;
 const app = express();
+
+app.use('/user', UserRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/routes/user.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import { Router } from 'express';
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.get('/:username', async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: req.params.username
+    }
+  });
+
+  await prisma.$disconnect();
+
+  if (!user) {
+    return res.status(404).json({ msg: 'User not found' });
+  }
+
+  return res.json(user);
+});
+
+export default router;
